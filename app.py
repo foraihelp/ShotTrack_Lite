b1d12900cdeb1b31983923f65ddbee0fbb5b6dc8
foraihelp <foraihelp26@gmail.com>
2026-03-16 15:00:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -370,5 +370,8 @@ def sequence(sid):
         shots=shots
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
